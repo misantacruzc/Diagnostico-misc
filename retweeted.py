537c7297e6dc85b32cf45921145e2cfbed9b6ed7
misantacruzc <misantacruzc@uc.cl>
2022-03-28 14:31:18 -0300

    retweeted

---
+++ retweeted.py
@@ -0,0 +1,25 @@
+import json
+
+
+def retweetCount(dict):
+    return dict['retweetCount']
+
+
+def retweeted():
+    top10 = []
+    for tweet in open('farmers-protest-tweets-2021-03-5.json', 'r'):
+        read_tweet = json.loads(tweet)
+        if len(top10) < 10:
+            top10.append(read_tweet)
+            top10.sort(reverse=True, key=retweetCount)
+        elif read_tweet['retweetCount'] >= top10[9]['retweetCount']:
+            top10.pop(9)
+            top10.append(read_tweet)
+            top10.sort(reverse=True, key=retweetCount)
+
+    for i in top10:
+        print(i['retweetCount'])
+    return top10
+
+
+retweeted()
